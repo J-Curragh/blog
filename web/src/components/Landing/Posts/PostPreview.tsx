d82ceda477284a6c05161df246b72f119c95b64d
J-Curragh <jcurragh03@gmail.com>
2022-07-06 19:46:29 +0100

    fix(web): Give each tag prop a key.

--- web/src/components/Landing/Posts/PostPreview.tsx
+++ web/src/components/Landing/Posts/PostPreview.tsx
@@ -30,7 +30,7 @@ const PostPreview = ({ post }: PostPreviewProps) => {
       <S.Title>
         <h2>{post.title}</h2>
         <S.TagsContainer>
-          {post.topics.map((topic) => <Tag topic={topic} colour={getTagColour(topic.id, currentTheme)} ></Tag>)}
+          {post.topics.map((topic) => <Tag key={topic.id} topic={topic} colour={getTagColour(topic.id, currentTheme)} ></Tag>)}
         </S.TagsContainer>
       </S.Title>
       <S.PreviewBody>{preview(post.content)}</S.PreviewBody>
